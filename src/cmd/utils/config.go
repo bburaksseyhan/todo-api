4dd9403f953f12d2ad997358df468a5d7c69dadb
bseyhan <60069987+bburaksseyhan@users.noreply.github.com>
2021-11-15 16:20:01 +0300

    change comment

--- src/cmd/utils/config.go
+++ src/cmd/utils/config.go
@@ -1,18 +1,18 @@
 package utils
 
-// Hold the all configurations in a single struct
+// Configuration hold the all configurations in a single struct
 type Configuration struct {
 	Database DBSettings
 	Host     HostSettings
 }
 
-// Cassandra DB Settings
+// DBSettings hold the Cassandra DB Settings
 type DBSettings struct {
 	Url      string
 	Keyspace string
 }
 
-// GIN Web Server Settings
+// HostSettings hold the GIN Web Server Settings
 type HostSettings struct {
 	Port string
 }
